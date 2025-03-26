a552967b5c74accaeb5f5f3a4cee0128b97b0535
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-03-26 16:45:13 +0900

    fix lint

--- src/screens/SearchScreen/modals/ScoreSelector.tsx
+++ src/screens/SearchScreen/modals/ScoreSelector.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+import CheckIcon from '@/assets/icon/ic_check.svg';
 import {color} from '@/constant/color.ts';
 import {font} from '@/constant/font.ts';
-import CheckBox from '@/components/common/CheckBox';
 
 export default function ScoreSelector({
   score,
@@ -60,4 +60,31 @@ const Label = styled.Text`
   font-size: 14px;
   font-family: ${font.pretendardMedium};
   color: ${color.gray100};
+`;
+
+function CheckBox({checked}: {checked: boolean}) {
+  if (checked) {
+    return (
+      <Check>
+        <CheckIcon color={color.white} width={20} height={20} />
+      </Check>
+    );
+  }
+  return <Uncheck />;
+}
+
+const Uncheck = styled.View`
+  width: 24px;
+  height: 24px;
+  border: 2px solid ${color.gray50};
+  border-radius: 8px;
+`;
+
+const Check = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  background-color: ${color.brandColor};
+  border-radius: 8px;
 `;
