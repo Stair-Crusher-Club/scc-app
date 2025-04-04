9efb6dd12adb2a343124151c251f08be6efc0820
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-05 01:01:48 +0900

    temp: temporarily disable sharing

--- src/utils/ShareUtils.ts
+++ src/utils/ShareUtils.ts
@@ -1,15 +1,17 @@
-import {Share} from 'react-native';
-
 import {Place} from '@/generated-sources/openapi';
+import ToastUtils from '@/utils/ToastUtils';
 
 const ShareUtils = {
   async sharePlace(place: Place) {
-    const url = `https://scc.airbridge.io/place/${place.id}`;
-    return Share.share({
-      title: place.name,
-      url,
-      message: `"${place.name}" 의 접근성 정보를 계단뿌셔클럽 앱에서 확인해보세요\n${url}`,
-    });
+    place;
+    ToastUtils.show('준비 중입니다.');
+    // TODO: universal link 이상 확인 후 복구하기
+    // const url = `https://scc.airbridge.io/place/${place.id}`;
+    // return Share.share({
+    //   title: place.name,
+    //   url,
+    //   message: `"${place.name}" 의 접근성 정보를 계단뿌셔클럽 앱에서 확인해보세요\n${url}`,
+    // });
   },
 };
 
