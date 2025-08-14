011c2a5ad7a380a2e6339aa9cb57da55ebf6550d
Zeniuus <jsh56son@gmail.com>
2025-08-14 15:36:02 +0900

    Fix: anonymous user가 남기는 이벤트에 userId가 남지 않는 문제 해결

--- src/hooks/useMe.ts
+++ src/hooks/useMe.ts
@@ -1,17 +1,10 @@
 import {useAtom, useSetAtom} from 'jotai';
 
 import {featureFlagAtom, userInfoAtom} from '@/atoms/Auth';
+import {ANONYMOUS_USER_TEMPLATE} from '@/constant/constant';
 import Logger from '@/logging/Logger';
 
 import useAppComponents from './useAppComponents';
-
-const GUEST_USER = {
-  id: '0',
-  email: '',
-  nickname: '둘러보는 중',
-  mobilityTools: [],
-  isNewsLetterSubscriptionAgreed: false,
-};
 
 export default function useMe() {
   const {api} = useAppComponents();
@@ -19,11 +12,20 @@ export default function useMe() {
   const setFeatureFlag = useSetAtom(featureFlagAtom);
 
   const syncUserInfo = async () => {
-    // 비회원으로 둘러보기를 선택한 경우, 유저 정보를 가져오지 못함
+    // 레거시; 과거에 비회원 유저의 id를 '0'으로 저장하던 시기를 처리하기 위한 로직.
     if (userInfo?.id === '0') {
-      setUserInfo(GUEST_USER);
+      setUserInfo(ANONYMOUS_USER_TEMPLATE);
       return;
     }
+
+    // 비회원 유저이더라도 userInfo.id는 올바르게 채번되어 있으므로 Logger에 userId를 세팅해준다.
+    await Logger.setUserId(userInfo?.id!);
+
+    // 이후 작업은 IDENTIFIED 유저에 대해서만 진행한다.
+    if (userInfo?.nickname === ANONYMOUS_USER_TEMPLATE.nickname) {
+      return;
+    }
+    
     // 이미 저장된 유저 정보가 있으면 상태 업데이트
     // 메인화면 진입 시마다 새로 유저 정보를 업데이트한다.
     const {data} = await api.getUserInfoGet();
@@ -32,7 +34,6 @@ export default function useMe() {
       isMapVisible: data.flags?.includes('MAP_VISIBLE') ?? false,
       isToiletVisible: data.flags?.includes('TOILET_VISIBLE') ?? false,
     });
-    await Logger.setUserId(data.user.id);
   };
 
   return {userInfo, syncUserInfo};
