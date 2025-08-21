df827c762773280282b5a6e7e461a0f724b4a874
ixio0330 <ixio0330@gmail.com>
2025-08-21 10:03:04 +0900

    feat: 이미지 타입 라벨 보여주기

--- src/screens/ImageZoomViewerScreen.tsx
+++ src/screens/ImageZoomViewerScreen.tsx
@@ -1,7 +1,7 @@
 import {useBackHandler} from '@react-native-community/hooks';
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {Pressable, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Image, Pressable, Text, View} from 'react-native';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import {IImageInfo} from 'react-native-image-zoom-viewer/built/image-viewer.type';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
@@ -9,16 +9,26 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import LeftArrowIcon from '@/assets/icon/ic_arrow_left.svg';
 import {color} from '@/constant/color';
 import {ScreenProps} from '@/navigation/Navigation.screens';
+import {
+  ImageType,
+  SlideText,
+} from './PlaceDetailScreen/sections/PlaceDetailCoverImage.style';
 
 export type ImageZoomViewerScreenParams = {
   imageUrls: string[];
   index?: number;
+  types?: string[];
 };
 
 const ImageZoomViewerScreen = ({route}: ScreenProps<'ImageZoomViewer'>) => {
   const navigation = useNavigation();
   const safeAreaInsets = useSafeAreaInsets();
-  const {imageUrls, index = 0} = route.params as ImageZoomViewerScreenParams;
+  const {
+    imageUrls,
+    index = 0,
+    types = [],
+  } = route.params as ImageZoomViewerScreenParams;
+  const [currentIndex, setCurrentIndex] = useState(index);
 
   useBackHandler(() => {
     navigation.goBack();
@@ -57,9 +67,20 @@ const ImageZoomViewerScreen = ({route}: ScreenProps<'ImageZoomViewer'>) => {
             </Text>
           );
         }}
+        onChange={i => setCurrentIndex(Number(i))}
         imageUrls={imageUrls.map((url: string) => {
           return {url} as IImageInfo;
         })}
+        renderImage={props => (
+          <View>
+            <Image {...props} />
+            {types.length > 0 && (
+              <ImageType>
+                <SlideText>{types[currentIndex]}</SlideText>
+              </ImageType>
+            )}
+          </View>
+        )}
       />
     </>
   );
