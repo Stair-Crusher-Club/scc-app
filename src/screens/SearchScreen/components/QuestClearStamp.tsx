af898c369768d7f3b488dac23232211c0abc09c2
ixio0330 <ixio0330@gmail.com>
2025-08-28 13:21:40 +0900

    feat: 로티 제거하고 스탬프 애니메이션 구현

--- src/screens/SearchScreen/components/QuestClearStamp.tsx
+++ src/screens/SearchScreen/components/QuestClearStamp.tsx
@@ -1,26 +1,126 @@
-import LottieView, {LottieViewProps} from 'lottie-react-native';
-import React from 'react';
+import {ChallengeQuestCompleteStampTypeDto} from '@/generated-sources/openapi';
+import React, {useEffect, useRef} from 'react';
+import {Animated, Image, ImageProps} from 'react-native';
 import styled from 'styled-components/native';
 
-export default function StampLottie(props: LottieViewProps) {
+const stampImage: Record<
+  ChallengeQuestCompleteStampTypeDto,
+  {
+    backgroundStamp: ImageProps['source'];
+    animatedStamp: ImageProps['source'];
+  }
+> = {
+  FLAG: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_flag_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_flag_animated.png'),
+  },
+  CAFE: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_cafe_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_cafe_animated.png'),
+  },
+  THUMBS_UP: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_good_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_good_animated.png'),
+  },
+  POTION: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_potion_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_potion_animated.png'),
+  },
+  RESTAURANT: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_restaurant_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_restaurant_animated.png'),
+  },
+  REVIEW: {
+    backgroundStamp: require('@/assets/img/stamp/stamp_review_bg.png'),
+    animatedStamp: require('@/assets/img/stamp/stamp_review_animated.png'),
+  },
+};
+
+interface QuestClearStampProps {
+  type?: ChallengeQuestCompleteStampTypeDto;
+  autoPlay?: boolean;
+  duration?: number;
+}
+
+export default function QuestClearStamp({
+  type = 'CAFE',
+  autoPlay = true,
+  duration = 600,
+}: QuestClearStampProps) {
+  const scaleValue = useRef(new Animated.Value(1.5)).current;
+  const opacityValue = useRef(new Animated.Value(0)).current;
+
+  const startStampAnimation = () => {
+    scaleValue.setValue(1.8);
+    opacityValue.setValue(0);
+
+    Animated.parallel([
+      Animated.timing(opacityValue, {
+        toValue: 1.0,
+        duration: duration * 0.15,
+        useNativeDriver: true,
+      }),
+      Animated.sequence([
+        Animated.timing(scaleValue, {
+          toValue: 0.9,
+          duration: duration * 0.15,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleValue, {
+          toValue: 1.0,
+          duration: duration * 0.75,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start();
+  };
+
+  useEffect(() => {
+    if (autoPlay) {
+      const timer = setTimeout(() => {
+        startStampAnimation();
+      }, 500);
+      return () => clearTimeout(timer);
+    }
+  }, [autoPlay]);
+
   return (
     <Container>
-      <LottieView
-        loop={false}
-        autoPlay
+      <BackgroundStamp source={stampImage[type].backgroundStamp} />
+
+      <AnimatedStampContainer
         style={{
-          width: 300,
-          height: 300,
-        }}
-        {...props}
-      />
+          transform: [{scale: scaleValue}],
+          opacity: opacityValue,
+        }}>
+        <AnimatedStamp source={stampImage[type].animatedStamp} />
+      </AnimatedStampContainer>
     </Container>
   );
 }
+
 const Container = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
   width: 300px;
   height: 300px;
+  position: relative;
+`;
+
+const BackgroundStamp = styled(Image)`
+  width: 150px;
+  height: 150px;
+  position: absolute;
+`;
+
+const AnimatedStampContainer = styled(Animated.View)`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AnimatedStamp = styled(Image)`
+  width: 150px;
+  height: 150px;
 `;
--- src/screens/SearchScreen/components/QuestCompletionModal.tsx
+++ src/screens/SearchScreen/components/QuestCompletionModal.tsx
@@ -1,10 +1,8 @@
 import {SccButton} from '@/components/atoms';
 import {color} from '@/constant/color';
 import {font} from '@/constant/font';
-import {ChallengeQuestCompleteStampTypeDto} from '@/generated-sources/openapi';
 import useNavigation from '@/navigation/useNavigation';
 import {useAtomValue, useSetAtom} from 'jotai';
-import {LottieViewProps} from 'lottie-react-native';
 import React, {useEffect} from 'react';
 import {Modal, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 import styled from 'styled-components/native';
@@ -16,19 +14,7 @@ import {
   nextOrCloseAtom,
   visibleAtom,
 } from '../atoms/quest';
-import StampLottie from './QuestClearStamp';
-
-const stampLottie: Record<
-  ChallengeQuestCompleteStampTypeDto,
-  LottieViewProps['source']
-> = {
-  FLAG: require('@/assets/animations/flag.lottie'),
-  CAFE: require('@/assets/animations/cafe.lottie'),
-  THUMBS_UP: require('@/assets/animations/good.lottie'),
-  POTION: require('@/assets/animations/potion.lottie'),
-  RESTAURANT: require('@/assets/animations/restaurant.lottie'),
-  REVIEW: require('@/assets/animations/review.lottie'),
-};
+import QuestClearStamp from './QuestClearStamp';
 
 export default function QuestCompletionModal() {
   const navigation = useNavigation();
@@ -71,7 +57,7 @@ export default function QuestCompletionModal() {
         <Backdrop>
           <Center>
             <CompletionImage />
-            <StampLottie source={stampLottie[current.type]} />
+            <QuestClearStamp type={current.type} />
           </Center>
 
           <TitleText>
@@ -86,9 +72,11 @@ export default function QuestCompletionModal() {
               onPress={handlePrimary}
             />
 
-            <TouchableOpacity onPress={closeAll}>
-              <CloseModalText>계속 정복하기</CloseModalText>
-            </TouchableOpacity>
+            {isLast && (
+              <TouchableOpacity onPress={closeAll}>
+                <CloseModalText>계속 정복하기</CloseModalText>
+              </TouchableOpacity>
+            )}
           </ButtonContainer>
         </Backdrop>
       </TouchableWithoutFeedback>
