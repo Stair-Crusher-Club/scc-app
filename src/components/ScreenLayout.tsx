5060880fd37555a4f787e7bfa86c33d0d4484386
Suhwan Jee <jsh56son@gmail.com>
2025-09-01 17:59:52 +0900

    안드로이드에서 KeyboardAvoidingView bottom이 잘못 잡히는 문제 해결 (#71)

--- src/components/ScreenLayout.tsx
+++ src/components/ScreenLayout.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {KeyboardAvoidingView, Platform, ViewProps} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Keyboard, KeyboardAvoidingView, Platform, ViewProps} from 'react-native';
 import {Edge, useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import {SafeAreaWrapper} from '@/components/SafeAreaWrapper';
@@ -21,10 +21,27 @@ export const ScreenLayout = ({
   style = {},
 }: ScreenLayoutProps) => {
   const insets = useSafeAreaInsets();
+  const [isKeyboardShownInAndroid, setIsKeyboardShownInAndroid] = useState<boolean>(false);
   let verticalOffset = 0;
   if (isHeaderVisible) {
     verticalOffset = HEIGHT_OF_NAVIGATION_HEADER + insets.top;
   }
+
+  // https://github.com/facebook/react-native/pull/51132
+  // 현재 RN 0.80 버전의 KeyboardAvoidingView의 경우, 안드로이드에서 키보드가 show / hide 될 때
+  // bottom padding / height가 올바르게 계산되지 않는 문제가 있다.
+  // 이를 해결해주기 위해, android에서는 키보드가 가려질 때 강제로 enabled를 false로 설정하게끔 한다.
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      const subscriptions = [
+        Keyboard.addListener('keyboardDidHide', () => setIsKeyboardShownInAndroid(false)),
+        Keyboard.addListener('keyboardDidShow', () => setIsKeyboardShownInAndroid(true)),
+      ]
+      return () => {
+        subscriptions.forEach(it => it.remove())
+      }
+    }
+  }, []);
 
   return (
     <SafeAreaWrapper
@@ -32,7 +49,7 @@ export const ScreenLayout = ({
       style={[{flex: 1, backgroundColor: color.white}, style]}>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        enabled={isKeyboardAvoidingView ?? true}
+        enabled={(isKeyboardAvoidingView && (Platform.OS !=='android' || isKeyboardShownInAndroid)) ?? true}
         keyboardVerticalOffset={verticalOffset}
         style={[{flex: 1}, style]}>
         {children}
