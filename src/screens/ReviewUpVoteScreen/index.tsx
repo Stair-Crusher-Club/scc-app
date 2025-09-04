450ecfabf2a1b02f6c2b3cbf9668c061191486bb
ixio0330 <ixio0330@gmail.com>
2025-09-04 17:09:03 +0900

    feat: 리뷰 도움이 되었어요 탭바 적용

--- src/screens/ReviewUpVoteScreen/index.tsx
+++ src/screens/ReviewUpVoteScreen/index.tsx
@@ -1,3 +1,16 @@
+import {ScreenLayout} from '@/components/ScreenLayout';
+import TabBar from '@/components/TabBar';
+import {useState} from 'react';
+import {tabItems} from '../ReviewScreen/constants';
+import {ReviewHistoryTab} from '../ReviewScreen/types';
+
 export default function ReviewUpVoteScreen() {
-  return null;
+  const [currentTab, setCurrentTab] = useState<ReviewHistoryTab>('place');
+
+  return (
+    <ScreenLayout isHeaderVisible={true}>
+      <TabBar items={tabItems} current={currentTab} onChange={setCurrentTab} />
+      {/* TODO: render items */}
+    </ScreenLayout>
+  );
 }
