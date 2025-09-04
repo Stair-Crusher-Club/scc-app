69a07b7a83b2029b4364d11e737b3136a132b621
ixio0330 <ixio0330@gmail.com>
2025-09-04 11:00:43 +0900

    chore: inset bottom 대응

--- src/screens/PlaceReviewFormScreen/index.tsx
+++ src/screens/PlaceReviewFormScreen/index.tsx
@@ -87,6 +87,7 @@ export default function PlaceReviewFormScreen({
   return (
     <LogParamsProvider params={{placeId}}>
       <ScreenLayout
+        safeAreaEdges={['bottom']}
         isHeaderVisible={true}
         style={{backgroundColor: color.white}}>
         {renderView()}
