92d0b531ec46efa9c34a2f4bebde1c019934ccec
ixio0330 <ixio0330@gmail.com>
2025-09-04 17:08:54 +0900

    feat: 지금까지 내가 작성한 리뷰 기본 구성

--- src/screens/ReviewHistoryScreen/index.tsx
+++ src/screens/ReviewHistoryScreen/index.tsx
@@ -1,3 +1,18 @@
+import {ScreenLayout} from '@/components/ScreenLayout';
+import TabBar from '@/components/TabBar';
+import {useState} from 'react';
+import AchievementsSection from '../ConquererHistoryScreen/sections/AchievementsSection';
+import {tabItems} from '../ReviewScreen/constants';
+import {ReviewHistoryTab} from '../ReviewScreen/types';
+
 export default function ReviewHistoryScreen() {
-  return null;
+  const [currentTab, setCurrentTab] = useState<ReviewHistoryTab>('place');
+
+  return (
+    <ScreenLayout isHeaderVisible={true}>
+      <TabBar items={tabItems} current={currentTab} onChange={setCurrentTab} />
+      <AchievementsSection type="review" totalNumberOfPlaces={0} />
+      {/* TODO: render items */}
+    </ScreenLayout>
+  );
 }
