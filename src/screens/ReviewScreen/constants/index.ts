fd377fe506068edf3f44fda151c9e71aa77cecaf
ixio0330 <ixio0330@gmail.com>
2025-09-04 17:08:27 +0900

    feat: 내 리뷰 메뉴

---
+++ src/screens/ReviewScreen/constants/index.ts
@@ -0,0 +1,12 @@
+import {ReviewHistoryTab} from '../types';
+
+export const tabItems = [
+  {
+    value: 'place' as ReviewHistoryTab,
+    label: '내부 이용 정보',
+  },
+  {
+    value: 'toilet' as ReviewHistoryTab,
+    label: '장애인 화장실 정보',
+  },
+];
