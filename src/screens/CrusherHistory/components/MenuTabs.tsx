3cf07430ab0bcab2aa33b39f2a4baf13cdd5453a
ixio0330 <ixio0330@gmail.com>
2025-09-04 14:53:33 +0900

    feat: 크러셔 활동 화면 탭 메뉴

---
+++ src/screens/CrusherHistory/components/MenuTabs.tsx
@@ -0,0 +1,60 @@
+import {color} from '@/constant/color';
+import {font} from '@/constant/font';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+export type Tab = 'current' | 'history';
+
+export const tabs: {
+  key: Tab;
+  value: string;
+}[] = [
+  {
+    key: 'current',
+    value: '현재시즌',
+  },
+  {
+    key: 'history',
+    value: '히스토리',
+  },
+];
+
+export default function MenuTabs({
+  currentTab,
+  setCurrentTab,
+}: {
+  currentTab: Tab;
+  setCurrentTab: (tab: Tab) => void;
+}) {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      {tabs.map(({key, value}) => (
+        <TouchableOpacity
+          key={key}
+          onPress={() => setCurrentTab(key)}
+          style={{
+            flex: 1,
+            borderBottomWidth: currentTab === key ? 2 : 1,
+            borderBottomColor:
+              currentTab === key ? color.brand50 : color.gray20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 12,
+          }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontFamily: font.pretendardMedium,
+              color: currentTab === key ? color.gray80 : color.gray40,
+            }}>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
