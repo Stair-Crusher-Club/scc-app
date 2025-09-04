dc958b53d01674b89201190c3d56736750a3d1b8
ixio0330 <ixio0330@gmail.com>
2025-09-04 17:17:57 +0900

    feat: 정복 도움이 되었어요 탭바 적용

---
+++ src/screens/ConquererUpVoteScreen/constants/index.ts
@@ -0,0 +1,12 @@
+import {ConquererUpVoteTab} from '../types';
+
+export const tabItems = [
+  {
+    value: 'place' as ConquererUpVoteTab,
+    label: '장소',
+  },
+  {
+    value: 'building' as ConquererUpVoteTab,
+    label: '건물',
+  },
+];
--- src/screens/ConquererUpVoteScreen/index.tsx
+++ src/screens/ConquererUpVoteScreen/index.tsx
@@ -1,3 +1,16 @@
+import {ScreenLayout} from '@/components/ScreenLayout';
+import TabBar from '@/components/TabBar';
+import {useState} from 'react';
+import {tabItems} from './constants';
+import {ConquererUpVoteTab} from './types';
+
 export default function ConquererUpVoteScreen() {
-  return null;
+  const [currentTab, setCurrentTab] = useState<ConquererUpVoteTab>('place');
+
+  return (
+    <ScreenLayout isHeaderVisible={true}>
+      <TabBar items={tabItems} current={currentTab} onChange={setCurrentTab} />
+      {/* TODO: render items */}
+    </ScreenLayout>
+  );
 }
