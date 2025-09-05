bdbcbc1e263505c366d9821f76c8d358fad1cec5
Zeniuus <jsh56son@gmail.com>
2025-09-05 12:46:48 +0900

    mount 시 element_view event 찍도록 다시 롤백

--- src/components/SccPressable.tsx
+++ src/components/SccPressable.tsx
@@ -2,7 +2,6 @@ import React, {forwardRef} from 'react';
 /* eslint-disable no-restricted-imports */
 import {Pressable, PressableProps, View} from 'react-native';
 /* eslint-enable no-restricted-imports */
-import {useOptimizedViewportVisibility} from '@/hooks/useOptimizedViewportVisibility';
 import {useSccEventLogging} from '@/hooks/useSccEventLogging';
 
 export interface SccPressableProps extends PressableProps {
@@ -12,34 +11,14 @@ export interface SccPressableProps extends PressableProps {
 
 export const SccPressable = forwardRef<View, SccPressableProps>(
   ({elementName, logParams, onPress, ...props}, ref) => {
-    const {viewRef, hasBeenVisible, checkVisibility} =
-      useOptimizedViewportVisibility(0.5);
-
     const {createPressHandler} = useSccEventLogging({
       elementName,
       logParams,
-      hasBeenVisible,
     });
 
     const handlePress = createPressHandler(onPress);
 
-    return (
-      <Pressable
-        ref={node => {
-          viewRef.current = node;
-          if (ref) {
-            if (typeof ref === 'function') {
-              ref(node);
-            } else {
-              ref.current = node;
-            }
-          }
-        }}
-        onPress={handlePress}
-        onLayout={checkVisibility}
-        {...props}
-      />
-    );
+    return <Pressable ref={ref} onPress={handlePress} {...props} />;
   },
 );
 
--- src/components/SccTouchableOpacity.tsx
+++ src/components/SccTouchableOpacity.tsx
@@ -2,7 +2,6 @@ import React, {forwardRef} from 'react';
 /* eslint-disable no-restricted-imports */
 import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 /* eslint-enable no-restricted-imports */
-import {useOptimizedViewportVisibility} from '@/hooks/useOptimizedViewportVisibility';
 import {useSccEventLogging} from '@/hooks/useSccEventLogging';
 
 export interface SccTouchableOpacityProps extends TouchableOpacityProps {
@@ -12,34 +11,14 @@ export interface SccTouchableOpacityProps extends TouchableOpacityProps {
 
 export const SccTouchableOpacity = forwardRef<any, SccTouchableOpacityProps>(
   ({elementName, logParams, onPress, ...props}, ref) => {
-    const {viewRef, hasBeenVisible, checkVisibility} =
-      useOptimizedViewportVisibility(0.5);
-
     const {createPressHandler} = useSccEventLogging({
       elementName,
       logParams,
-      hasBeenVisible,
     });
 
     const handlePress = createPressHandler(onPress);
 
-    return (
-      <TouchableOpacity
-        ref={node => {
-          viewRef.current = node;
-          if (ref) {
-            if (typeof ref === 'function') {
-              ref(node);
-            } else {
-              ref.current = node;
-            }
-          }
-        }}
-        onPress={handlePress}
-        onLayout={checkVisibility}
-        {...props}
-      />
-    );
+    return <TouchableOpacity ref={ref} onPress={handlePress} {...props} />;
   },
 );
 
