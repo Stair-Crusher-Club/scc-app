4af3f998b4dfc3b72ebbb6844936a743c4e01baf
Zeniuus <jsh56son@gmail.com>
2025-09-12 10:26:52 +0900

    정규 버전에 의존하기

--- src/screens/HomeScreen/sections/ChallengeSection.tsx
+++ src/screens/HomeScreen/sections/ChallengeSection.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {ScrollView} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 
