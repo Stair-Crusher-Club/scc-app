4300d854cef3e8d9ac75b4005d8ea35bc2acf3be
ixio0330 <ixio0330@gmail.com>
2025-09-18 12:44:52 +0900

    feat: 내가 작성한 리뷰

---
+++ src/screens/ReviewHistoryScreen/components/PlaceToiletReviewItem.tsx
@@ -0,0 +1,219 @@
+import dayjs from 'dayjs';
+import React, {useState} from 'react';
+import {Alert, View} from 'react-native';
+import styled from 'styled-components/native';
+
+import {SccTouchableOpacity} from '@/components/SccTouchableOpacity';
+
+import MoreIcon from '@/assets/icon/ic_more.svg';
+import {color} from '@/constant/color';
+import {font} from '@/constant/font';
+import {doorTypeMap} from '@/constant/options';
+import {TOILET_LOCATION_TYPE_LABELS} from '@/constant/review';
+import {ToiletReviewListItemDto} from '@/generated-sources/openapi';
+import ImageList from '@/screens/PlaceDetailScreen/components/PlaceDetailImageList';
+import ToastUtils from '@/utils/ToastUtils';
+
+import {useDeleteReview} from '../../PlaceDetailScreen/hooks/useDeleteReview';
+import DeleteBottomSheet from '../../PlaceDetailScreen/modals/DeleteBottomSheet';
+
+export default function PlaceToiletReviewItem({
+  placeId,
+  review,
+}: {
+  placeId: string;
+  review: ToiletReviewListItemDto;
+}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const deleteToiletReview = useDeleteReview({
+    type: 'toilet',
+    reviewId: review.toiletReviewId,
+    placeId,
+  });
+  const reviewImages = review.images;
+  const reviewText = review.comment;
+  const reviewDate = dayjs(review.createdAt.value).format('YYYY.MM.DD');
+
+  return (
+    <Container>
+      <HeaderRow>
+        <HeaderLeft>
+          <PlaceName>{review.placeName}</PlaceName>
+          <PlaceAddress>{review.placeAddress}</PlaceAddress>
+        </HeaderLeft>
+        {review.isDeletable && (
+          <SccTouchableOpacity
+            elementName="place_toilet_review_more_button"
+            onPress={() =>
+              Alert.alert(
+                '더보기',
+                '',
+                [
+                  {
+                    text: '삭제',
+                    onPress: () => setIsDeleteModalVisible(true),
+                    style: 'destructive',
+                  },
+                  {
+                    text: '수정',
+                    onPress: () => ToastUtils.show('준비중입니다.'),
+                  },
+                  {text: '취소', style: 'cancel'},
+                ],
+                {cancelable: true},
+              )
+            }>
+            <MoreIcon />
+          </SccTouchableOpacity>
+        )}
+      </HeaderRow>
+      <ReviewContentColumn style={{width: '100%'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            gap: 12,
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          <ReviewInfoColumn>
+            <ReviewInfoRow>
+              <ReviewInfoLabel>등록일</ReviewInfoLabel>
+              <ReviewInfoValue>{reviewDate}</ReviewInfoValue>
+            </ReviewInfoRow>
+            <ReviewInfoRow>
+              <ReviewInfoLabel>위치</ReviewInfoLabel>
+              <ReviewInfoValue>
+                {TOILET_LOCATION_TYPE_LABELS[review.toiletLocationType]}
+              </ReviewInfoValue>
+            </ReviewInfoRow>
+            {review.floor && (
+              <ReviewInfoRow>
+                <ReviewInfoLabel>층</ReviewInfoLabel>
+                <ReviewInfoValue>{review.floor}</ReviewInfoValue>
+              </ReviewInfoRow>
+            )}
+
+            {review.entranceDoorTypes &&
+              review.entranceDoorTypes?.length > 0 && (
+                <ReviewInfoRow>
+                  <ReviewInfoLabel>문유형</ReviewInfoLabel>
+                  <ReviewInfoValue>
+                    {review.entranceDoorTypes
+                      ?.map(type => doorTypeMap[type])
+                      .join(', ')}
+                  </ReviewInfoValue>
+                </ReviewInfoRow>
+              )}
+          </ReviewInfoColumn>
+          {reviewImages && reviewImages.length > 0 && (
+            <ImageList
+              images={reviewImages || []}
+              roundCorners
+              isSinglePreview
+            />
+          )}
+        </View>
+        {reviewText && (
+          <>
+            <ReviewText numberOfLines={isExpanded ? undefined : 2}>
+              {reviewText}
+            </ReviewText>
+            {reviewText.length > 50 && ( // TODO 정확히 2줄 넘으면 안보이게 하기
+              <ExpandButton
+                elementName="toilet_review_expand_button"
+                onPress={() => setIsExpanded(value => !value)}>
+                <ExpandButtonText>
+                  {isExpanded ? '접기' : '더보기'}
+                </ExpandButtonText>
+              </ExpandButton>
+            )}
+          </>
+        )}
+      </ReviewContentColumn>
+
+      <DeleteBottomSheet
+        isVisible={isDeleteModalVisible}
+        confirmText={'화장실 리뷰를 정말 삭제할까요?'}
+        onPressCancelButton={() => setIsDeleteModalVisible(false)}
+        onPressConfirmButton={() => {
+          setIsDeleteModalVisible(false);
+          deleteToiletReview.mutate();
+        }}
+      />
+    </Container>
+  );
+}
+
+// styled-components
+const Container = styled.View`
+  gap: 16px;
+  flex-direction: column;
+`;
+const HeaderRow = styled.View`
+  flex-direction: row;
+  gap: 4px;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.View`
+  flex-direction: column;
+  gap: 4px;
+  flex: 1;
+`;
+const PlaceName = styled.Text`
+  font-size: 16px;
+  line-height: 24px;
+  font-family: ${font.pretendardBold};
+  color: ${color.gray90};
+`;
+const PlaceAddress = styled.Text`
+  font-size: 13px;
+  line-height: 18px;
+  font-family: ${font.pretendardRegular};
+  color: ${color.gray50};
+`;
+const ReviewContentColumn = styled.View`
+  flex-direction: column;
+  gap: 8px;
+`;
+const ReviewInfoColumn = styled.View`
+  flex-direction: column;
+  gap: 2px;
+  align-items: flex-start;
+`;
+const ReviewInfoRow = styled.View`
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+`;
+const ReviewInfoLabel = styled.Text`
+  font-size: 12px;
+  line-height: 16px;
+  font-family: ${font.pretendardRegular};
+  width: 42px;
+  color: ${color.gray40};
+`;
+const ReviewInfoValue = styled.Text`
+  font-size: 14px;
+  line-height: 22px;
+  font-family: ${font.pretendardRegular};
+  color: ${color.gray70};
+`;
+const ReviewText = styled.Text`
+  font-size: 15px;
+  line-height: 22px;
+  font-family: ${font.pretendardRegular};
+  color: ${color.gray70};
+`;
+const ExpandButton = styled(SccTouchableOpacity)`
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-end;
+`;
+const ExpandButtonText = styled.Text`
+  font-size: 13px;
+  line-height: 18px;
+  font-family: ${font.pretendardRegular};
+  color: ${color.gray40};
+`;
--- src/screens/ReviewHistoryScreen/index.tsx
+++ src/screens/ReviewHistoryScreen/index.tsx
@@ -1,18 +1,114 @@
+import EmptyViewText from '@/components/empty/EmptyViewText';
+import FeedbackButton from '@/components/FeedbackButton';
 import {ScreenLayout} from '@/components/ScreenLayout';
 import TabBar from '@/components/TabBar';
+import {color} from '@/constant/color';
+import useAppComponents from '@/hooks/useAppComponents';
+import {FlashList} from '@shopify/flash-list';
+import {useInfiniteQuery} from '@tanstack/react-query';
 import {useState} from 'react';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
 import AchievementsSection from '../ConquererHistoryScreen/sections/AchievementsSection';
 import {tabItems} from '../ReviewScreen/constants';
 import {ReviewHistoryTab} from '../ReviewScreen/types';
+import PlaceReviewItem from './components/PlaceReviewItem';
+import PlaceToiletReviewItem from './components/PlaceToiletReviewItem';
 
 export default function ReviewHistoryScreen() {
+  const {api} = useAppComponents();
   const [currentTab, setCurrentTab] = useState<ReviewHistoryTab>('place');
+
+  const {data, fetchNextPage, hasNextPage, isFetchingNextPage} =
+    useInfiniteQuery({
+      queryKey: ['ReviewList', currentTab],
+      queryFn: async ({pageParam}) => {
+        if (currentTab === 'place') {
+          return (
+            await api.listRegisteredPlaceReviewsPost({
+              nextToken: pageParam,
+              limit: 20,
+            })
+          ).data;
+        } else {
+          return (
+            await api.listRegisteredToiletReviewsPost({
+              nextToken: pageParam,
+              limit: 20,
+            })
+          ).data;
+        }
+      },
+      getNextPageParam: lastPage => {
+        return lastPage.nextToken;
+      },
+      initialPageParam: undefined as string | undefined,
+    });
+
+  const totalNumberOfReviews = data?.pages[0].totalNumberOfItems ?? 0;
 
   return (
     <ScreenLayout isHeaderVisible={true}>
       <TabBar items={tabItems} current={currentTab} onChange={setCurrentTab} />
-      <AchievementsSection type="review" totalNumberOfPlaces={0} />
-      {/* TODO: render items */}
+      <AchievementsSection
+        type="review"
+        totalNumberOfPlaces={totalNumberOfReviews}
+      />
+
+      <FlashList
+        data={
+          data?.pages.flatMap(page => {
+            if (currentTab === 'place') {
+              return (page as any).placeReviews;
+            } else {
+              return (page as any).toiletReviews;
+            }
+          }) ?? []
+        }
+        renderItem={({item}) => {
+          if (currentTab === 'place') {
+            return (
+              <>
+                <View
+                  style={{
+                    padding: 20,
+                    gap: 16,
+                  }}>
+                  <PlaceReviewItem placeId={item.placeId} review={item} />
+                  <FeedbackButton />
+                </View>
+                <Divider />
+              </>
+            );
+          } else {
+            return (
+              <>
+                <View
+                  style={{
+                    padding: 20,
+                    gap: 16,
+                  }}>
+                  <PlaceToiletReviewItem placeId={item.placeId} review={item} />
+                  <FeedbackButton />
+                </View>
+                <Divider />
+              </>
+            );
+          }
+        }}
+        onEndReached={() => {
+          if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+          }
+        }}
+        onEndReachedThreshold={0.5}
+        ListEmptyComponent={<EmptyViewText>{/* TODO */}</EmptyViewText>}
+      />
     </ScreenLayout>
   );
 }
+
+const Divider = styled.View`
+  height: 1px;
+  background-color: ${color.gray20};
+`;
