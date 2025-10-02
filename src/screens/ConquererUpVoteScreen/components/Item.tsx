7302158cc5dbc09c18c9d9197684553ab2caadd4
ixio0330 <ixio0330@gmail.com>
2025-10-02 20:26:38 +0900

    feat: 내가 정복한 장소 도움이 되었어요 목록 API 연동

---
+++ src/screens/ConquererUpVoteScreen/components/Item.tsx
@@ -0,0 +1,75 @@
+import FeedbackButton from '@/components/FeedbackButton';
+import SccPressable from '@/components/SccPressable';
+import {color} from '@/constant/color';
+import {font} from '@/constant/font';
+import {UpvotedPlaceDto} from '@/generated-sources/openapi/api';
+import {useUpvoteToggle} from '@/hooks/useUpvoteToggle';
+import useNavigation from '@/navigation/useNavigation';
+import {Text, View} from 'react-native';
+
+interface ItemProps {
+  item: UpvotedPlaceDto;
+}
+
+export default function Item({item}: ItemProps) {
+  const navigation = useNavigation();
+
+  const targetType = item.accessibilityType!!;
+  const targetId = item.accessibilityId!!;
+
+  const {isUpvoted, totalUpvoteCount, toggleUpvote} = useUpvoteToggle({
+    initialIsUpvoted: item.isUpvoted,
+    initialTotalCount: item.totalUpvoteCount,
+    targetId,
+    targetType,
+    placeId: item.id!!,
+  });
+
+  return (
+    <View style={{gap: 16}}>
+      <SccPressable
+        elementName="navigate_to_place_detail"
+        onPress={() =>
+          navigation.navigate('PlaceDetail', {
+            placeInfo: {
+              placeId: item.id!!,
+            },
+          })
+        }
+        style={{
+          gap: 4,
+        }}>
+        <Text
+          style={{
+            fontSize: 16,
+            fontFamily: font.pretendardBold,
+            lineHeight: 24,
+            color: color.gray90,
+          }}>
+          {item.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: 13,
+            fontFamily: font.pretendardRegular,
+            lineHeight: 18,
+            color: color.gray50,
+          }}>
+          {item.address}
+        </Text>
+      </SccPressable>
+
+      <FeedbackButton
+        total={totalUpvoteCount}
+        upvoted={isUpvoted}
+        onPressUpvote={toggleUpvote}
+        onPressAnalytics={() => {
+          navigation.navigate('UpvoteAnalytics', {
+            targetType,
+            targetId,
+          });
+        }}
+      />
+    </View>
+  );
+}
--- src/screens/ConquererUpVoteScreen/index.tsx
+++ src/screens/ConquererUpVoteScreen/index.tsx
@@ -1,18 +1,74 @@
+import EmptyViewText from '@/components/empty/EmptyViewText';
 import {ScreenLayout} from '@/components/ScreenLayout';
-import TabBar from '@/components/TabBar';
-import {UpvoteTargetTypeDto} from '@/generated-sources/openapi';
-import {useState} from 'react';
-import {tabItems} from './constants';
+import {color} from '@/constant/color';
+import useAppComponents from '@/hooks/useAppComponents';
+import {FlashList} from '@shopify/flash-list';
+import {useInfiniteQuery} from '@tanstack/react-query';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+import Item from './components/Item';
 
 export default function ConquererUpvoteScreen() {
-  const [currentTab, setCurrentTab] = useState<UpvoteTargetTypeDto>(
-    'PLACE_ACCESSIBILITY',
-  );
+  const {api} = useAppComponents();
+
+  const {data, fetchNextPage, hasNextPage, isFetchingNextPage} =
+    useInfiniteQuery({
+      queryKey: ['PlacesUpvoted'],
+      queryFn: async ({pageParam}) => {
+        const result = await api.listUpvotedPlacesPost({
+          nextToken: pageParam,
+          limit: 20,
+        });
+        console.log(result);
+        return result.data;
+      },
+      getNextPageParam: lastPage => {
+        return lastPage.nextToken;
+      },
+      initialPageParam: undefined as string | undefined,
+    });
+
+  const places = data?.pages.flatMap(page => page.places ?? []) ?? [];
 
   return (
-    <ScreenLayout isHeaderVisible={true}>
-      <TabBar items={tabItems} current={currentTab} onChange={setCurrentTab} />
-      {/* TODO: render items */}
+    <ScreenLayout isHeaderVisible={false}>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: color.gray20,
+        }}
+      />
+      <FlashList
+        data={places}
+        renderItem={({item}) => (
+          <>
+            <View
+              style={{
+                padding: 20,
+              }}>
+              <Item item={item} />
+            </View>
+            <Divider />
+          </>
+        )}
+        onEndReached={() => {
+          if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+          }
+        }}
+        onEndReachedThreshold={0.5}
+        ListEmptyComponent={<EmptyViewText>{/* TODO */}</EmptyViewText>}
+        ListFooterComponent={<PaddingBottom />}
+      />
     </ScreenLayout>
   );
 }
+
+const Divider = styled.View`
+  height: 1px;
+  background-color: ${color.gray20};
+`;
+
+const PaddingBottom = styled.View`
+  padding-bottom: 100px;
+`;
