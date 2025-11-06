582874fad15e7246eab17f9455d4c2fa46dd8aae
ixio0330 <ixio0330@gmail.com>
2025-11-06 20:56:06 +0900

    chore: 등록 완료 모달에 사용할 임시 상수 추가

---
+++ src/screens/RegistrationCompleteScreen/constants/index.ts
@@ -0,0 +1,15 @@
+export const REGISTRATION_COMPLETE_CONTENT = {
+  place: {
+    title: '장소 정보 등록 완료!',
+    description: '등록해주신 정보가 많은 분들에게 도움이 될 거예요.',
+    // TODO: img_registration_complete_place.png 파일 추가 필요
+    imagePath: require('@/assets/img/register_success_astronaut.png'),
+  },
+  building: {
+    title: '건물 정보 등록 완료!',
+    description: '등록해주신 정보가 많은 분들에게 도움이 될 거예요.',
+    // TODO: img_registration_complete_building.png 파일 추가 필요
+    imagePath: require('@/assets/img/register_success_astronaut.png'),
+  },
+} as const;
+
