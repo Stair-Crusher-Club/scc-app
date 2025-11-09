9dfb39acb0ade6873147e56f16d9bd8e64ef5245
ixio0330 <ixio0330@gmail.com>
2025-11-09 20:11:13 +0900

    chore: 임시 이미지 경로

--- src/screens/RegistrationCompleteScreen/constants/index.ts
+++ src/screens/RegistrationCompleteScreen/constants/index.ts
@@ -2,14 +2,13 @@ export const REGISTRATION_COMPLETE_CONTENT = {
   place: {
     title: '장소 정보 등록 완료!',
     description: '등록해주신 정보가 많은 분들에게 도움이 될 거예요.',
-    // TODO: img_registration_complete_place.png 파일 추가 필요
-    imagePath: require('@/assets/img/register_success_astronaut.png'),
+    // FIXME
+    imagePath: require('@/assets/img/img_crusher_welcome.png'),
   },
   building: {
     title: '건물 정보 등록 완료!',
     description: '등록해주신 정보가 많은 분들에게 도움이 될 거예요.',
-    // TODO: img_registration_complete_building.png 파일 추가 필요
-    imagePath: require('@/assets/img/register_success_astronaut.png'),
+    // FIXME
+    imagePath: require('@/assets/img/img_crusher_welcome.png'),
   },
 } as const;
-
