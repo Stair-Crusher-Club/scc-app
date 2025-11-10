6e2ab265f6e8d1a94843b15c6e798ca0551975de
Zeniuus <jsh56son@gmail.com>
2025-11-10 17:39:39 +0900

    Fix: iOS 공유하기 시 메세지가 두 번 나가는 문제 해결

--- src/utils/ShareUtils.ts
+++ src/utils/ShareUtils.ts
@@ -6,8 +6,6 @@ const ShareUtils = {
   async sharePlace(place: Place) {
     const url = `https://link.staircrusher.club/c8yi3t?placeId=${place.id}`;
     return Share.share({
-      title: place.name,
-      url,
       message: `[${place.name}]의 접근성 정보를 계단뿌셔클럽 앱에서 확인해보세요!\n${url}`,
     });
   },
