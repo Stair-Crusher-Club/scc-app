5273eb50d3294c21d4b306872d8eccd310ce16c3
ixio0330 <ixio0330@gmail.com>
2025-11-20 15:34:52 +0900

    feat: 장소 정보 툴팁에서 뱃지로 변경

--- src/screens/PlaceReviewFormScreen/sections/PlaceInfoSection.tsx
+++ src/screens/PlaceReviewFormScreen/sections/PlaceInfoSection.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-import NewConquerTargetTooltip from '@/components/NewConquerTargetTooltip';
+import IcBuilding from '@/assets/icon/ic_building.svg';
+import IcPlace from '@/assets/icon/ic_place.svg';
 import {color} from '@/constant/color';
 import {font} from '@/constant/font';
 
@@ -16,9 +17,19 @@ export default function PlaceInfoSection({
   address,
   target,
 }: PlaceInfoSectionProps) {
+  const Icon = target === 'place' ? IcPlace : IcBuilding;
+  const badgeText =
+    target === 'place' ? '새로운 장소 발견!' : '새로운 건물 발견!';
+  const badgeColor = target === 'place' ? color.yellow70 : color.brand50;
+
   return (
     <Container>
-      {target && <NewConquerTargetTooltip target={target} />}
+      {target && (
+        <Badge>
+          <Icon width={16} height={16} />
+          <BadgeText style={{color: badgeColor}}>{badgeText}</BadgeText>
+        </Badge>
+      )}
       <InfoContainer>
         <Title>{name}</Title>
         <Address>{address}</Address>
@@ -32,6 +43,18 @@ const Container = styled.View({
   backgroundColor: color.white,
   alignItems: 'flex-start',
   gap: 8,
+});
+
+const Badge = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: 4,
+});
+
+const BadgeText = styled.Text({
+  fontSize: 14,
+  lineHeight: '20px',
+  fontFamily: font.pretendardBold,
 });
 
 const InfoContainer = styled.View({
