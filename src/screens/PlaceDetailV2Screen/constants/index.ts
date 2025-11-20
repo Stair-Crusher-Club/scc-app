e8fb3e940969dbe90adce4e67bd136e981bb79e9
ixio0330 <ixio0330@gmail.com>
2025-11-20 15:21:55 +0900

    fix: 버튼명 수정

--- src/screens/PlaceDetailV2Screen/constants/index.ts
+++ src/screens/PlaceDetailV2Screen/constants/index.ts
@@ -4,16 +4,16 @@ export const BUILDING_REGISTRATION_CONTENT = {
     description:
       '매장에 가기 위해 건물 입구 정보가 필요해요.\n등록해주실 수 있나요?',
     imagePath: require('@/assets/img/form/building_suggest_strong.png'),
-    confirmButtonText: '나중에 등록하기',
-    cancelButtonText: '지금 등록하기',
+    confirmButtonText: '지금 등록하기',
+    cancelButtonText: '나중에 등록하기',
   },
   'registration-suggest': {
     title: '건물정보도 등록해보세요!',
     description:
       '1층이 아닌 다른층에 있는 장소네요?\n건물 정보도 등록해보시는거 어때요?',
     imagePath: require('@/assets/img/form/building_suggest_weak.png'),
-    confirmButtonText: '나중에 등록하기',
-    cancelButtonText: '지금 등록하기',
+    confirmButtonText: '지금 등록하기',
+    cancelButtonText: '나중에 등록하기',
   },
 } as const;
 
